f4c8975c1fad9988198f702edef764f6ae7e44ef
a-msy <m5s2y5a@gmail.com>
2020-10-17 23:02:42 +0900

    toTop

--- component/footer.php
+++ component/footer.php
@@ -6,6 +6,9 @@ $kenkos = file_get_contents($getcategory, false, $GLOBALS['context']);
 $arr = json_decode($kenkos, true);
 ?>
 </main>
+<div class="position-fixed upper" style="right:5%;bottom:5%;transition:1s;opacity:0.7;z-index:100;">
+    <img src="img/btn--page-top.png">
+</div>
 <footer class="mt-5" style="background-color: #e7e7e7; word-break: break-all;">
     <div class="container">
         <div class="row">
@@ -89,8 +92,8 @@ $arr = json_decode($kenkos, true);
         </div>
     </div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
         integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
@@ -108,6 +111,11 @@ $arr = json_decode($kenkos, true);
         const spinner = document.getElementById('loading');
         spinner.classList.remove('loaded');
     }
+</script>
+<script>
+    $(".upper").on('click',function(){
+        $('html,body').animate({'scrollTop':0},500);
+    });
 </script>
 </body>
 </html>
